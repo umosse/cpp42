51cfd267f6a3872915c3c9914e55e316e5f8c4cd
Ulysse Mosse <umosse@made-f0Cr2s6.clusters.42paris.fr>
2024-11-13 14:50:24 +0100

    poulet

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umosse <umosse@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 13:16:42 by umosse            #+#    #+#             */
+/*   Updated: 2024/11/13 14:50:07 by umosse           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+# include <fstream>
+# include <string>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 4)
+	{
+		std::fstream	FileName;
+		std::fstream	NewFileName;
+		std::string		argv1 = argv[1];
+		std::string		argv2 = argv[2];
+		std::string		argv3 = argv[3];
+		FileName.open(argv1, std::ios::in);
+		if (!FileName)
+			std::cout << "Cannot open file" << std::endl;
+		else
+		{
+			NewFileName.open(argv1 + ".replace", std::ios::out);
+			if (!NewFileName)
+				std::cout << "Error creating file" << std::endl;
+			else
+			{
+				std::cout << "File created" << std::endl;
+				std::string	str;
+				while (std::getline(FileName, str))
+				{
+					std::size_t	offset = 0;
+					std::size_t	pos = 0;
+					pos = str.find(argv2, offset);
+					while (pos != std::string::npos)
+					{
+						str.erase(pos, argv2.size());
+						str.insert(pos, argv3);
+						offset = pos + argv3.size();
+						pos = str.find(argv2, offset);
+					}
+					NewFileName << str << std::endl;
+				}
+				NewFileName.close();
+			}
+			FileName.close();
+		}
+	}
+	return (0);
+}
