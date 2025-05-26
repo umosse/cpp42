fc2e9f1d09e48f144b3417ce1d0f57e0a6b4cc5e
Ulysse Mosse <umosse@f6r3s8.clusters.42paris.fr>
2025-05-26 16:07:37 +0200

    poulet

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,6 +1,10 @@
 #include "ScalarCoonverter.hpp"
 #include <cctype>
+#include <cmath>
+#include <iomanip>
+#include <ios>
 #include <iostream>
+#include <limits>
 #include <string>
 #include <cstdlib>
 
@@ -27,14 +31,52 @@ void ScalarConverter::convert(std::string s)
 	int		i = static_cast<int>(d);
 	char	c = static_cast<char>(d);
 
-	if (i < )
-	if (std::isprint(i))
-		std::cout << "char: " << c << "\n";
+	if (std::isnan(d) && s.length() == 3)
+	{
+		std::cout << "char: impossible\nint: impossible\nfloat: nanf\ndouble: nan\n";
+		return ;
+	}
+	if (std::isinf(d) && s.length() == 3)
+	{
+		std::cout << "char: impossible\nint: impossible\nfloat: inff\ndouble: inf\n";
+		return ;
+	}
 
-	std::cout << "int: " << i << "\n";
-	std::cout << "float: " << f << "\n";
-	std::cout << "double: " << d << "\n";
-	//overflow : std::numeric limit > renvoie le max/min de la string > atoi et comparer si la string a changee.
-	//nan : std::isnan
-	//inf : std::isinf
+	bool	go = false;
+	for (size_t i = 0; i < s.length(); i++)
+	{
+		if (std::isdigit(s[i]))
+		go = true;
+	}
+
+	if (go)
+	{
+		if (i < 0 || i > 127)
+			std::cout << "char: impossible\n";
+		else if (std::isprint(i))
+			std::cout << "char: " << c << "\n";
+		else
+			std::cout << "char: Non displayable\n";
+		if (std::numeric_limits<int>::max() < d || (std::numeric_limits<int>::min() > d))
+		{
+			std::cout << "int: impossible\n";
+			go = false;
+		}
+		size_t	dot = s.find('.');
+		size_t	precision = 1;
+		if (dot != std::string::npos)
+		{
+			precision = s.length() - dot - 1;
+			if (precision > 5)
+				precision = 5;
+		}
+		if (go)
+			std::cout << "int: " << i << "\n";
+		std::cout << std::fixed << std::setprecision(precision) << "float: " << f << "f\n";
+		std::cout << std::setprecision(0) << "double: " << d << "\n";
+	}
+	else
+	{
+		std::cout << "char: impossible\nint: impossible\nfloat: impossible\ndouble: impossible\n";;
+	}
 }
