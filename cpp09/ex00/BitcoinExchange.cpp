c6e914103c8fed4bcc4a1dc8d30db643071dc8d6
Ulysse Mosse <umosse@f2r2s2.clusters.42paris.fr>
2025-07-01 16:52:47 +0200

    poulet

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -1,2 +1,36 @@
 #include "BitcoinExchange.hpp"
 
+BitcoinExchange::BitcoinExchange()
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+	std::cout << "Default destructor called" << std::endl;
+}
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
+{
+	if (this != &other)
+	{
+		
+	}
+	std::cout << "Assignment operator called" << std::endl;
+	return (*this);
+}
+
+void	BitcoinExchange::inputParsing(std::string input)
+{
+
+}
+
+void	BitcoinExchange::dbParsing(std::string path)
+{
+
+}
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -1,10 +1,25 @@
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
-class {
-	public:
+#include <iostream>
 
+struct Date {
+	int	year;
+	int	month;
+	int	day;
+};
+
+class BitcoinExchange{
+	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange &other);
+		~BitcoinExchange();
+		BitcoinExchange& operator=(const BitcoinExchange &other);
+
+		void	inputParsing(std::string input);
+		void	dbParsing(std::string path);
 	private:
+
 };
 
 #endif
